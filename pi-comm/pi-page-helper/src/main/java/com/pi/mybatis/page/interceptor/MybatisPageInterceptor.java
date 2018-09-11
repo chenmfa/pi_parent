09e247faa3418ea89d7470861be5bb0f6f42b8d9
chenmfa <790109677@qq.com>
2018-09-12 07:48:10 +0800

    构建mybatis拦截器

---
+++ pi-comm/pi-page-helper/src/main/java/com/pi/mybatis/page/interceptor/MybatisPageInterceptor.java
@@ -0,0 +1,48 @@
+package com.pi.mybatis.page.interceptor;
+
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pi.mybatis.page.model.IPageInfo;
+
+@Intercepts(value = {
+    @Signature(type=Executor.class, method="query", 
+        args={MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
+})
+public class MybatisPageInterceptor implements Interceptor{
+  private static final Logger logger = LoggerFactory.getLogger(MybatisPageInterceptor.class);
+  @Override
+  public Object intercept(Invocation invocation) throws Throwable {
+    Object[] args = invocation.getArgs();// 方法参数
+    Method method = invocation.getMethod();//拦截方法
+    Object target = invocation.getTarget();//拦截原始类
+    return null;
+  }
+
+  @Override
+  public Object plugin(Object target) {
+    return Plugin.wrap(target, this);
+  }
+
+  @Override
+  public void setProperties(Properties properties) {
+//    do nothing
+  }
+  
+  private IPageInfo wrapParamToPageInfo(Object args){
+    return null;
+  }
+
+}
---
+++ pi-comm/pi-page-helper/src/main/java/com/pi/mybatis/page/model/IPageInfo.java
@@ -0,0 +1,6 @@
+package com.pi.mybatis.page.model;
+
+public interface IPageInfo {
+  public Integer getPageNumber();
+  public Integer getPageSize();
+}
